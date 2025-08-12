e4af147f0882ba763163cdc82456e64d4e0a41cd
Taffytheking06 <davidtmusk06@gmail.com>
2025-08-12 18:59:22 +0530

    One to one relationship

--- store/models.py
+++ store/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     last_updated = models.DateTimeField(auto_now_=True)  
 
 
-class customer(models.Model):
+class Customer(models.Model):
         MEMBERSHIP_BRONZE = 'B'
         MEMBERSHIP_SILVER = 'S'
         MEMBERSHIP_GOLD = 'G'
@@ -39,7 +39,13 @@ class Order(models.Model):
       placed_at = models.DateTimeField(auto_now_add=True)
       payment_status = models.CharField(
             max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
-    
+      
+
+      class Address(models.Model):
+            street = models.CharField(max_length=255)
+            city = models.CharField(max_length=255)
+            customers = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+           
 
     
     
